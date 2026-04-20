2c9d710846bcd5a955b272162d3aacd329ee981d
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-20 14:13:51 +0200

    feat: import validate from validateMiddleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { validate } from "../middleware/validateRequest.js";
 import { registerSchema, loginSchema } from "../validations/userValidation.js";
 
 const router = express.Router();
