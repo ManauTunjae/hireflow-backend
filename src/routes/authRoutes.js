c94a72aa8aa412fd10f6ff2a2aec1c0676a79bee
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-20 14:43:07 +0200

    feat: apply express-validator to auth routes and remove unused zod middleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
-import { registerSchema, loginSchema } from "../validations/userValidation.js";
+import {
+  validateRegister,
+  validateLogin,
+} from "../validations/userValidation.js";
 
 const router = express.Router();
 
-router.post("/register", registerSchema, registerUser);
-router.post("/login", loginSchema, loginUser);
+router.post("/register", validateRegister, registerUser);
+router.post("/login", validateLogin, loginUser);
 
 export default router;
