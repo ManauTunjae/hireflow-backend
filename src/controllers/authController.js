d58d62cd07c22dda666460ed027be8f7a11d7211
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-20 09:52:29 +0200

    feat: add registerUser logic with role handling and password hashing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,32 @@
 import User from "../models/User";
 
-
+// Skapa en ny användare
+export async function registerUser(req, res) {
+  try {
+    const { username, email, password, role, company } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({
+      username,
+      email,
+      passwordHash: password,
+      role,
+      userRole,
+      company,
+    });
+    res.status(201).json({
+      message: "User registered sucsessfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        company: user.company,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", err: err.message });
+  }
+}
