4fbbbb779298a8ab20dbb20839e340f8f2bc93c1
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-20 13:14:58 +0200

    feat: implement global validateRequest middleware

---
+++ src/middleware/validateRequest.js
@@ -0,0 +1,20 @@
+export const validate = (schema) => (req, res, next) => {
+    try {
+        // Här validera body, query och params mot schemat
+        schema.parse({
+            body: req.body,
+            query: req.query,
+            params: req.params
+        });
+        next();
+    } catch (error) {
+        return res.status(400).json({ 
+            message: "Validation failed", 
+            errors: error.error.map((e) => ({
+                field: e.path[1],
+                message: e.message,
+            }))
+        });
+    }
+};
+ 
