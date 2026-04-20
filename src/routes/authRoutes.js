a95fa4470be4f2a3505dcf5f530a5d031158a792
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-20 14:34:37 +0200

    chore: install express-validator

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
-import { validate } from "../middleware/validateRequest.js";
 import { registerSchema, loginSchema } from "../validations/userValidation.js";
 
 const router = express.Router();
 
-router.post("/register", validate(registerSchema), registerUser);
-router.post("/login", validate(loginSchema), loginUser);
+router.post("/register", registerSchema, registerUser);
+router.post("/login", loginSchema, loginUser);
 
 export default router;
