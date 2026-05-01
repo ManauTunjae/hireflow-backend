8681bc66996fe0760effb26d9a871b32b994ec89
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-01 08:18:39 +0200

    feat: create Job model with recruiter reference

---
+++ src/models/Job.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { required } from "zod/mini";
+
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    company: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["open", "closed", "draft"],
+      default: "open",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    salary: {
+      type: String, // t.ex. "50 000 - 70 000 SEK/månad"
+      required: true,
+    },
+    requirements: [String], // En lista med krav (t.ex. ["React", "Node.js"])
+  },
+  { timestamps: true },
+);
