7be787afde557db9ef72d524acfd1e01ea22388d
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-01 09:10:51 +0200

    feat: implement job validation rules

--- src/validations/jobValidation.js
+++ src/validations/jobValidation.js
@@ -1,9 +1,58 @@
-import {body, validationResult} from 'express-validator';
+import { body, validationResult } from "express-validator";
 
 const handleValidationErrors = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: error.array() });
-    }
-    next(); 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: error.array() });
+  }
+  next();
 };
+
+export const validateJob = [
+  body("title")
+    .isLength({ min: 2 })
+    .notEmpty()
+    .withMessage("Title must be at least 2 characters long")
+    .isString()
+    .withMessage("Title must be a string")
+    .trim(),
+
+  body("description")
+    .isLength({ min: 10 })
+    .notEmpty()
+    .withMessage("Description must be at least 10 characters long")
+    .isString()
+    .withMessage("Description must be a string")
+    .trim(),
+
+  body("company")
+    .isLength({ min: 2 })
+    .notEmpty()
+    .withMessage("Company name must be at least 2 characters long")
+    .isString()
+    .withMessage("Company name must be a string")
+    .trim(),
+
+  body("location")
+    .isLength({ min: 2 })
+    .notEmpty()
+    .withMessage("Location must be at least 2 characters long")
+    .isString()
+    .withMessage("Location must be a string")
+    .trim(),
+
+  body("status").isIn(["open", "closed"]).withMessage("Invalid status value"),
+
+  body("salary")
+    .notEmpty()
+    .withMessage("Salary is required")
+    .isString()
+    .withMessage("Salary must be a string. Ex. 40,000 - 50,000 SEK/month"),
+
+  body("requirements")
+    .optional()
+    .isArray()
+    .withMessage("Requirements must be an array"),
+
+  handleValidationErrors,
+];
