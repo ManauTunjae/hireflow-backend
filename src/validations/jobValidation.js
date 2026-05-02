b36c9ac478b38181864f78add3955791173d292b
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-02 19:38:05 +0200

    fix: handleValidationErrors

--- src/validations/jobValidation.js
+++ src/validations/jobValidation.js
@@ -1,9 +1,9 @@
 import { body, validationResult } from "express-validator";
 
 const handleValidationErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: error.array() });
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.status(400).json({ errors: result.array() });
   }
   next();
 };
