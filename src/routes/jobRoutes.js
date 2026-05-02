9c49f4ad7836b5b7e23b890354ddd7d0e6dc95cd
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-02 20:02:45 +0200

    feat: implement public getJobs and private getJobs endpoints

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createJob } from "../controllers/jobController.js";
+import { createJob, getJobs } from "../controllers/jobController.js";
 import { validateJob } from "../validations/jobValidation.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, validateJob, createJob);
+router.get("/", getJobs); // Alla kan se alla jobb
+router.get("/my-jobs", authMiddleware, getJobs); // HR kan se sina egna jobb
 
 export default router;
