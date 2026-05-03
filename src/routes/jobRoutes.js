b71b7752b4f338bfaf5c9dde713defdaf6fe82ea
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-03 14:19:06 +0200

    feat: implement PUT jobs/:id endpoint for update a job with authorization

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createJob, getJobs } from "../controllers/jobController.js";
+import { createJob, getJobs, updateJob } from "../controllers/jobController.js";
 import { validateJob } from "../validations/jobValidation.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/", authMiddleware, validateJob, createJob);
 router.get("/", getJobs); // Alla kan se alla jobb
 router.get("/my-jobs", authMiddleware, getJobs); // HR kan se sina egna jobb
+router.put("/:id", authMiddleware, validateJob, updateJob);
 
 export default router;
