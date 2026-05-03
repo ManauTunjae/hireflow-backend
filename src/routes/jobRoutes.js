176b0194c74a4ef61aa44c5a6a2b37fccbcb182c
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-03 14:28:17 +0200

    feat: import validateJobUpdate and use it with PUT endpoint

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { createJob, getJobs, updateJob } from "../controllers/jobController.js";
-import { validateJob } from "../validations/jobValidation.js";
+import {
+  validateJob,
+  validateJobUpdate,
+} from "../validations/jobValidation.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -8,6 +11,6 @@ const router = express.Router();
 router.post("/", authMiddleware, validateJob, createJob);
 router.get("/", getJobs); // Alla kan se alla jobb
 router.get("/my-jobs", authMiddleware, getJobs); // HR kan se sina egna jobb
-router.put("/:id", authMiddleware, validateJob, updateJob);
+router.put("/:id", authMiddleware, validateJobUpdate, updateJob);
 
 export default router;
