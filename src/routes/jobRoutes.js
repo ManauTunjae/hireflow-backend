c7f3200cf3a72fbe9bdff55afda2ee6dc70913aa
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-03 14:43:46 +0200

    feat: import deleteJob into jobRoutes.js

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createJob, getJobs, updateJob } from "../controllers/jobController.js";
+import {
+  createJob,
+  getJobs,
+  updateJob,
+  deleteJob,
+} from "../controllers/jobController.js";
 import {
   validateJob,
   validateJobUpdate,
@@ -12,5 +17,6 @@ router.post("/", authMiddleware, validateJob, createJob);
 router.get("/", getJobs); // Alla kan se alla jobb
 router.get("/my-jobs", authMiddleware, getJobs); // HR kan se sina egna jobb
 router.put("/:id", authMiddleware, validateJobUpdate, updateJob);
+router.delete("/:id", authMiddleware, deleteJob);
 
 export default router;
