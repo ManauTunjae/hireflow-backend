f3df3f8b9f27af3bd382da1e21e88c094d1041ab
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-09 22:40:33 +0200

    feat: implement DELETE /:id to delete a candidate

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -4,6 +4,7 @@ import {
   getAllCandidates,
   getCandidateById,
   updateCandidate,
+  deleteCandidate,
 } from "../controllers/candidateController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
@@ -13,5 +14,6 @@ router.post("/", authMiddleware, createCandidate);
 router.get("/", authMiddleware, getAllCandidates);
 router.get("/:id", authMiddleware, getCandidateById);
 router.patch("/:id", authMiddleware, updateCandidate);
+router.delete("/:id", authMiddleware, deleteCandidate);
 
 export default router;
