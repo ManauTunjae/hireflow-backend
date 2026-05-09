32d2e74e88bce142aeb4b835131301d60fb4e408
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-09 23:15:16 +0200

    feat: implement updateStatus function to validate candidate status

--- src/validations/candidateValidation.js
+++ src/validations/candidateValidation.js
@@ -1,9 +1,37 @@
 import { body, validationResult } from "express-validator";
 
 export const handleValidationErrors = (req, res, next) => {
-  const error = validationResult(req);
-  if (!error) {
-    return res.status * (404).json({ error: error.array() });
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.status(404).json({ error: result.array() });
   }
   next();
 };
+
+export const validateCandidate = [
+  body("name")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Candidate name must be at least 3 characters long"),
+
+  body("email").trim().isEmail().withMessage("Invalid email format"),
+
+  body("phone").trim().notEmpty().withMessage("Phone number is required"),
+
+  body("jobId")
+    .notEmpty()
+    .withMessage("Job ID is required")
+    .isMongoId()
+    .withMessage("Invalid Hob ID format"),
+
+  handleValidationErrors,
+];
+
+export const updateStatus = [
+  body("status")
+    .optional()
+    .isIn(["applied", "interviewing", "hired", "rejected"])
+    .withMessage("Invalid status value"),
+
+  handleValidationErrors,
+];
