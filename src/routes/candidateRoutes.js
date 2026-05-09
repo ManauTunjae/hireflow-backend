3b23a6ad784af4b6876765421cfeda011069999f
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-09 23:15:46 +0200

    feat: import validations and implement to POST and PATCH

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -7,13 +7,17 @@ import {
   deleteCandidate,
 } from "../controllers/candidateController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import {
+  validateCandidate,
+  updateStatus,
+} from "../validations/candidateValidation.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, createCandidate);
+router.post("/", authMiddleware, validateCandidate, createCandidate);
 router.get("/", authMiddleware, getAllCandidates);
 router.get("/:id", authMiddleware, getCandidateById);
-router.patch("/:id", authMiddleware, updateCandidate);
+router.patch("/:id", authMiddleware, updateStatus, updateCandidate);
 router.delete("/:id", authMiddleware, deleteCandidate);
 
 export default router;
