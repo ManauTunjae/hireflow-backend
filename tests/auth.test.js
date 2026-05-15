d24d0e0593d54d6e0ae1e4b882d659abf25dd92b
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-15 22:22:39 +0200

    feat: implement negative login test for invalid credentials

---
+++ tests/auth.test.js
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+import app from "../src/server.js";
+import { response } from "express";
+
+describe("Auth API Integration Tests", () => {
+  it("Should return 200 if login credentials are wrong", async () => {
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "icke-existerande@test.com", password: "fel-losenord" });
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toHaveProperty('message');
+  });
+});
