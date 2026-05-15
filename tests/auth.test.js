ac219b4ca3706479ff86895563264a047636a221
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-15 22:42:26 +0200

    test: implement and verify integration tests for login and registration duplicates

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -9,6 +9,25 @@ describe("Auth API Integration Tests", () => {
       .post("/api/auth/login")
       .send({ email: "icke-existerande@test.com", password: "fel-losenord" });
     expect(response.statusCode).toBe(401);
-    expect(response.body).toHaveProperty('message');
+    expect(response.body).toHaveProperty("message");
+  });
+
+  it("Should return 400 if trying to register en email that already exists", async () => {
+    await request(app).post("/api/auth/register-candidate").send({
+      username: "Manau",
+      email: "alreadyexists@test.com",
+      password: "password123",
+    });
+
+    const response = await request(app)
+      .post("/api/auth/register-candidate")
+      .send({
+        username: "Gustav",
+        email: "alreadyexists@test.com",
+        password: "gustav-password",
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("message");
   });
 });
