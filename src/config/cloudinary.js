84a08b08444bf072a2801104269d2a9b506964a4
Manau Tunjae <manau.tunjae@hotmail.com>
2026-06-23 18:05:50 +0200

    Add 'type' parameter for Cloudinary uploads

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -18,6 +18,7 @@ const storage = new CloudinaryStorage({
   params: {
     folder: "candidate_files",
     resource_type: "auto",
+    type: "upload",
     allowed_formats: ["pdf", "jpg", "png", "doc", "docx"],
   },
 });
